ba0d4cda205cf32ad2d8f36348d7daa9b2d3755d
Carlos Rosales <carlos.rosales0615@gmail.com>
2020-08-31 19:25:42 -0500

    Router definition.

--- web/app/views/app.js
+++ web/app/views/app.js
@@ -1,27 +1,28 @@
 ﻿angularApp.config([
-  "$routeProvider",
-  function ($routeProvider) {
-    $routeProvider
-      .when("/", {
-        controller: "mainCtrl",
-        templateUrl: "/app/views/landing/landingView.html",
-        controller: "landingController",
-      })
-      .when("/login", {
-        templateUrl: "/app/views/login/loginView.html",
-        controller: "loginController",
-        data: {},
-      })
-      .when("/techs", {
-        templateUrl: "/app/views/tech-list/techListView.html",
-        controller: "techListController",
-        data: {},
-      })
-      .when("/unauthorized", {
-        templateUrl: "/app/shared/unauthorized.html",
-      })
-      .otherwise({
-        redirectTo: "/",
-      });
-  },
+	'$routeProvider',
+	'$locationProvider',
+	function($routeProvider, $locationProvider) {
+		$locationProvider.html5Mode(true);
+		$routeProvider
+			.when('/', {
+				templateUrl: '/app/views/landing/landingView.html',
+				controller: 'landingController'
+			})
+			.when('/login', {
+				templateUrl: '/app/views/login/loginView.html',
+				controller: 'loginController',
+				data: {}
+			})
+			.when('/techs', {
+				templateUrl: '/app/views/tech-list/techListView.html',
+				controller: 'techListController',
+				data: {}
+			})
+			.when('/unauthorized', {
+				templateUrl: '/app/shared/unauthorized.html'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	}
 ]);
