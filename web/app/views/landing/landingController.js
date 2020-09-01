c45d23e4c55f0a9ec89fa9eca36a1e554b5a2f9b
Carlos Rosales <carlos.rosales0615@gmail.com>
2020-08-31 21:49:37 -0500

    If the user has a token, ignore login.

--- web/app/views/landing/landingController.js
+++ web/app/views/landing/landingController.js
@@ -3,8 +3,38 @@
 	'$location',
 	'$window',
 	function($scope, $location, $window) {
+		$scope.benefits = [
+			{
+				path: '../../../assets/img/Ic_Hour.svg',
+				description: 'Flexibilidad\nHoraria'
+			},
+			{
+				path: '../../../assets/img/IC_HomeOffice.svg',
+				description: 'Home Office'
+			},
+			{
+				path: '../../../assets/img/Ic_Workshops.svg',
+				description: 'Capacitaciones\ny workshops'
+			},
+			{
+				path: '../../../assets/img/Ic_DrinkSnacks.svg',
+				description: 'Snacks, frutas\ny bebidas gratis'
+			},
+			{
+				path: '../../../assets/img/Ic_laptop.svg',
+				description: 'Semana\nRemota'
+			},
+			{
+				path: '../../../assets/img/Ic_brain.svg',
+				description: 'Trabajar\nen ultimas\ntecnologías'
+			}
+		];
+		$scope.authorization = localStorage.getItem('authorization');
 		$scope.toLogin = function() {
 			$location.path('/login');
+		};
+		$scope.toTechList = function() {
+			$location.path('/techs');
 		};
 		$scope.redirectToTwitter = function() {
 			$window.open('https://twitter.com/Wolox', '_blank');
