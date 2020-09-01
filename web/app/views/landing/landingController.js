1d9608e74464cac8ec64dba7083c24abe34d0edd
Carlos Rosales <carlos.rosales0615@gmail.com>
2020-08-31 19:25:27 -0500

    Landing pages logic.

--- web/app/views/landing/landingController.js
+++ web/app/views/landing/landingController.js
@@ -1,1 +1,16 @@
-﻿angularApp.controller("landingController", ["$scope", function ($scope) {}]);
+﻿angularApp.controller('landingController', [
+	'$scope',
+	'$location',
+	'$window',
+	function($scope, $location, $window) {
+		$scope.toLogin = function() {
+			$location.path('/login');
+		};
+		$scope.redirectToTwitter = function() {
+			$window.open('https://twitter.com/Wolox', '_blank');
+		};
+		$scope.redirectToWolox = function() {
+			$window.open('https://www.wolox.com.ar/', '_blank');
+		};
+	}
+]);
